77693bd54673af3f84ee16abba96a790b7d3f914
Richard Gobert <dev.richard.g@gmail.com>
2019-09-25 00:17:38 +0200

    add variable player1,2 , p1Result,p2Result and buttom reste

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-
+let player1 = document.getElementById("p1");
+let player2 = document.getElementById("p2");
+let resetButton = document.getElementById("reset");
+let p1Result = document.getElementById("p1Result");
+let p2Result = document.getElementById("p2Result");
 
