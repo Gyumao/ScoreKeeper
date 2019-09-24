072600055e8e2eedd2ab92615479ad37928ea0eb
Richard Gobert <dev.richard.g@gmail.com>
2019-09-25 00:25:15 +0200

    add variable p1,p2Score , gameOver and ScoreToWin

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ let player2 = document.getElementById("p2");
 let resetButton = document.getElementById("reset");
 let p1Result = document.getElementById("p1Result");
 let p2Result = document.getElementById("p2Result");
-
+let p1Score = 0;
+let p2Score = 0;
+let gameOver = false;
+let winningScore = 5;
